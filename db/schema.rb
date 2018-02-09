2403e94832ab9598b59dfe5c3e22ea6ee27d7496
Rafael <rafael@rafael.lan>
2018-02-09 05:02:04 +0100

    Création Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207142935) do
+ActiveRecord::Schema.define(version: 20180209035248) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180207142935) do
     t.string "place"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "events_users", id: false, force: :cascade do |t|
